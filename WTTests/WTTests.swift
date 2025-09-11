0c8aa9a83dba4f9cbf51d7a23c8b2b65c4d984a8
April Stocker <astocke@jacksonville.edu>
2025-09-11 16:41:32 -0400

    Initial Commit

---
+++ WTTests/WTTests.swift
@@ -0,0 +1,17 @@
+//
+//  WTTests.swift
+//  WTTests
+//
+//  Created by April on 9/11/25.
+//
+
+import Testing
+@testable import WT
+
+struct WTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
